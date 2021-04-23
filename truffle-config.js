d6c3e56252e80c2bb5bbbfec5f573a5119ac038b
sompote <sompote.r@gmail.com>
2021-04-23 14:51:51 +0700

    add config for bsc

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
-require('dotenv').config;
+require('dotenv').config();
 
 module.exports = {
   // Uncommenting the defaults below
@@ -24,12 +24,27 @@ module.exports = {
       port: 8545,
       network_id: '*',
     },
+    testnet: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.PRIVATE_KEY,
+          `https://data-seed-prebsc-1-s1.binance.org:8545`
+        ),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
     bsc: {
       provider: () =>
         new HDWalletProvider(
           process.env.PRIVATE_KEY,
-          'https://data-seed-prebsc-2-s1.binance.org:8545'
+          `https://bsc-dataseed1.binance.org`
         ),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
 };
